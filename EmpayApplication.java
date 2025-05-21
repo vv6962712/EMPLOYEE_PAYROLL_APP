91d318d22f9b689858468e7d0b23c23c1b9d614f
Vishnu <vv6962712@gmail.com>
2025-05-21 10:18:29 +0530

    Initial Commit

---
+++ EmpayApplication.java
@@ -0,0 +1,13 @@
+package com.example.empay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpayApplication.class, args);
+	}
+
+}
